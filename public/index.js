bdb58c411974c14c190a72618dc0d1b06c2bac5e
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2021-06-10 13:01:59 -0700

    Manually copying files from tojo-js-testing to current branch

--- public/index.js
+++ public/index.js
@@ -3,15 +3,27 @@
 function handleNameSubmit() {
 	var lead_list = document.getElementById("leaderboard_list");
 	var input = document.getElementById("username");
-	
-	if (input.value != "") {
-		
+
+	if (!input){
+		alert("Please input a name!")
+	} else {
+		var req = new XMLHttpRequest()
+		var reqName = "/user/add";
+		req.open('POST', reqName)
+
+		var person = {
+			name: input.value,
+		}
+
+		var reqBody = JSON.stringify(person)
+		console.log(" == reqBody:", reqBody)
+		req.setRequestHeader('Content-Type', 'application/json')
+		req.send(reqBody)
 
 		handleLeaderboards(input.value);
+		sortLeaderboards()
 		input.value = "";
 		hideModal();
-	} else {
-		alert("Please input a name!")
 	}
 }
 
@@ -55,6 +67,8 @@ function modifyLeaderboards(name, score, index) {
 	var lead_list_arr = lead_list.getElementsByTagName("li");
 	var new_str = name + ": " + score;
 
+	console.log("== list:", lead_list_arr[1])
+
 	//create a new node to be inserted into the DOM
 	var new_ref = document.createElement("li")
 	var temp_content = document.createTextNode(new_str)
@@ -71,6 +85,55 @@ function modifyLeaderboards(name, score, index) {
 		lead_list.insertBefore(new_ref, lead_list_arr[index]);
 
 	}
+}
+
+//TESTING SORT
+function sortLeaderboards() {
+	var separator = ": ";
+	var parent = document.getElementById("leaderboard_list")
+	var lead_list = parent.getElementsByTagName("li")
+
+	var name_arr = []
+	var score_arr = []
+	var sorted = false
+
+	for (entry of lead_list) {
+		let str = entry.textContent
+		name_arr.push(str.split(separator)[0])
+		score_arr.push(str.split(separator)[1])
+	}
+
+	while(!sorted) {
+		sorted = true;
+		for (i = 0; i < name_arr.length; i++) {
+			if (i-1 >= 0) {
+				if (score_arr[i-1] < score_arr[i]) {
+					sorted = false;
+					let sc_temp = score_arr[i-1]
+					score_arr[i-1] = score_arr[i]
+					score_arr[i] = sc_temp
+
+					let nm_temp = name_arr[i-1]
+					name_arr[i-1] = name_arr[i]
+					name_arr[i] = nm_temp
+				}
+			}
+		}
+	}
+
+    while (parent.lastChild) {
+      parent.removeChild(parent.lastChild);
+    }
+
+	for (i = 0; i < name_arr.length; i++) {
+		var new_ref = document.createElement("li")
+		var new_str = name_arr[i] + separator + score_arr[i]
+		console.log(new_str)
+		var content = document.createTextNode(new_str)
+		new_ref.appendChild(content)
+		parent.appendChild(new_ref)
+	}
+
 }
 
 function showModal() {
@@ -104,4 +167,3 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 
 })
-
--- public/leaderboardTemplate.js
+++ public/leaderboardTemplate.js
@@ -1,6 +1,17 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['leaderboard'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<li>testName: testscore</li>";
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<li>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"key") || (data && lookupProperty(data,"key"))) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"key","hash":{},"data":data,"loc":{"start":{"line":1,"column":4},"end":{"line":1,"column":12}}}) : helper)))
+    + ": "
+    + alias4(((helper = (helper = lookupProperty(helpers,"score") || (depth0 != null ? lookupProperty(depth0,"score") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"score","hash":{},"data":data,"loc":{"start":{"line":1,"column":14},"end":{"line":1,"column":23}}}) : helper)))
+    + "</li>";
 },"useData":true});
 })();
