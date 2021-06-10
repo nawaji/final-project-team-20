bd26f922483a384dde02092ac7744cbc10c42da4
nawaji <Jonathant15191@gmail.com>
2021-06-10 02:06:41 -0700

    Implemented functions for sorting, but index.js does not fully utilize it

--- public/index.js
+++ public/index.js
@@ -3,14 +3,7 @@
 function handleNameSubmit() {
 	var lead_list = document.getElementById("leaderboard_list");
 	var input = document.getElementById("username");
-	
-//	if (input.value != "") {
-//		handleLeaderboards(input.value);
-//		input.value = "";
-//		hideModal();
-//	} else {
-//		alert("Please input a name!")
-//	}
+
 	if (!input){
 		alert("Please input a name!")
 	} else {
@@ -20,13 +13,15 @@ function handleNameSubmit() {
 
 		var person = {
 			name: input.value,
-			score: 1
 		}
+
 		var reqBody = JSON.stringify(person)
 		console.log(" == reqBody:", reqBody)
 		req.setRequestHeader('Content-Type', 'application/json')
 		req.send(reqBody)
 
+		sortLeaderboards()
+		
 		handleLeaderboards(input.value);
 		input.value = "";
 		hideModal();
@@ -73,6 +68,8 @@ function modifyLeaderboards(name, score, index) {
 	var lead_list_arr = lead_list.getElementsByTagName("li");
 	var new_str = name + ": " + score;
 
+	console.log("== list:", lead_list_arr[1])
+
 	//create a new node to be inserted into the DOM
 	var new_ref = document.createElement("li")
 	var temp_content = document.createTextNode(new_str)
@@ -89,6 +86,55 @@ function modifyLeaderboards(name, score, index) {
 		lead_list.insertBefore(new_ref, lead_list_arr[index]);
 
 	}
+}
+
+//TESTING SORT
+function sortLeaderboards() {
+	var separator = ": ";
+	var parent = document.getElementById("leaderboard_list")
+	var lead_list = parent.getElementsByTagName("li")
+
+	var name_arr = []
+	var score_arr = []
+	var sorted = false
+
+	for (entry of lead_list) {
+		let str = entry.textContent
+		name_arr.push(str.split(separator)[0])
+		score_arr.push(str.split(separator)[1])
+	}
+
+	while(!sorted) {
+		sorted = true;
+		for (i = 0; i < name_arr.length; i++) {
+			if (i-1 >= 0) {
+				if (score_arr[i-1] < score_arr[i]) {
+					sorted = false;
+					let sc_temp = score_arr[i-1]
+					score_arr[i-1] = score_arr[i]
+					score_arr[i] = sc_temp
+
+					let nm_temp = name_arr[i-1]
+					name_arr[i-1] = name_arr[i]
+					name_arr[i] = nm_temp
+				}
+			}
+		}
+	}
+
+    while (parent.lastChild) {
+      parent.removeChild(parent.lastChild);
+    }
+
+	for (i = 0; i < name_arr.length; i++) {
+		var new_ref = document.createElement("li")
+		var new_str = name_arr[i] + separator + score_arr[i]
+		console.log(new_str)
+		var content = document.createTextNode(new_str)
+		new_ref.appendChild(content)
+		parent.appendChild(new_ref)
+	}
+
 }
 
 function showModal() {
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var user_data = require("./leaderboards.json");
 
 console.log(user_data);
 
+
 var app = express();
 var port = process.env.PORT || 3001;
 
@@ -39,8 +40,6 @@ app.get(["/ttt", "/"], function (req, res, next) {
 app.post('/user/add', function (req, res, next) {
 	var person = req.body
 	console.log("== person:", person)
-	var score = person.score
-	console.log("== score:", score)
 
 	//read leaderboards.json
 	//check for duplicate name
@@ -49,6 +48,7 @@ app.post('/user/add', function (req, res, next) {
 
 		//update name/score if exists already
 		user_data[person.name].score++
+		sortUserData()
 
 		fs.writeFile(__dirname + "/leaderboards.json",
 		JSON.stringify(user_data, null, 2),
@@ -56,11 +56,13 @@ app.post('/user/add', function (req, res, next) {
 			if(err){
 				res.status(500).send("Error writing new data. Try again")
 			} else{
+//				console.log("before sort funct")
+//				sortUserData()
 				res.status(200).send()
 			}			
 		})
 
-//else push new name/score to leaderboards.json
+	//else push new name/score to leaderboards.json
 	} else {
 		console.log("new user")
 
@@ -80,9 +82,50 @@ app.post('/user/add', function (req, res, next) {
 	}
 })
 
+function sortUserData() {
+	var i = 0
+	var name_arr = []
+	var score_arr = []
+	var sorted = false;
+
+	//separate "name" and "score" into two different arrays
+	for (key in user_data) {
+		name_arr.push(key)
+		score_arr.push(user_data[key].score)
+	}
+
+	//sort these two arrays based on a person's score
+	while(!sorted) {
+		sorted = true;
+		for (i = 0; i < name_arr.length; i++) {
+			if (i-1 >= 0) {
+				if (score_arr[i-1] < score_arr[i]) {
+					sorted = false;
+					let sc_temp = score_arr[i-1]
+					score_arr[i-1] = score_arr[i]
+					score_arr[i] = sc_temp
+
+					let nm_temp = name_arr[i-1]
+					name_arr[i-1] = name_arr[i]
+					name_arr[i] = nm_temp
+				}
+			}
+		}
+	}
+	//rewrite our user data, recombining name and score arrays
+	user_data = {}
+	for (i = 0; i < name_arr.length; i++) {
+		user_data[name_arr[i]] = {
+			"score": score_arr[i]
+		}
+	}
+
+	console.log(user_data)
+}
+
 app.get('*', function (req, res, next) {
 
-	res.status(404).render ('404');
+	res.status(404).render('404');
 
 })
 
