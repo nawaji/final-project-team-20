01917d331ad172d710f21d27c969ccefacac38b9
HyunJ-OregonState <kimh6@oregonstate.edu>
2021-06-10 16:10:53 -0700

    Server Update

--- server.js
+++ server.js
@@ -1,3 +1,58 @@
+// /* 
+//  * NOTE: This is a basic server.js file to test client-side/server-side interactions
+//  * with index.js/html whenever it's added
+//  */
+
+// var fs = require("fs")
+// var express = require("express");
+// var user_data = require("./leaderboards.json");
+// //var bodyParser = require("body-parser")
+
+// console.log(user_data);
+
+// var app = express();
+// var port = process.env.PORT || 3001;
+
+// //test
+
+// //app.use(bodyParser.json())
+// app.use(express.json())
+// app.use(express.static('public'));
+
+// app.post("/leaderboards/addEntry", function(req, res) {
+// 	console.log("== req.body:", req.body)
+// 	res.status(200).send()
+// 	console.log("== user_data[" + person + "]:", user_data[person])
+// })
+
+
+// app.get("/ttt", function (req, res, next) {
+// 	res.status(200).sendFile(__dirname + '/public/index.html');
+// })
+
+// //Sort LeaderBoard
+// var i;
+// var j;
+// var key;
+// for(i = 0; i < user_data.length();i++){
+// 	key = user_data[i].score
+// 	for(j = i-1; j >= 0; j--){
+// 		if(user_data[j].score > key){
+// 			user_data[j+1] = user_data[j]
+// 		}
+// 	}
+// 	user_data[j+1].score = key
+// }
+
+// app.listen(port, function () {
+// 	console.log("== Server is listening on port", port);
+// })
+
+/* 
+ * NOTE: This is a basic server.js file to test client-side/server-side interactions
+ * with index.js/html whenever it's added
+ */
+
 /* 
  * NOTE: This is a basic server.js file to test client-side/server-side interactions
  * with index.js/html whenever it's added
@@ -5,13 +60,18 @@
 
 var fs = require("fs")
 var express = require("express");
+var exphbs = require("express-handlebars");
 var user_data = require("./leaderboards.json");
 //var bodyParser = require("body-parser")
 
 console.log(user_data);
 
+
 var app = express();
 var port = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 //test
 
@@ -19,32 +79,106 @@ var port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.static('public'));
 
-app.post("/leaderboards/addEntry", function(req, res) {
-	console.log("== req.body:", req.body)
-	res.status(200).send()
-	console.log("== user_data[" + person + "]:", user_data[person])
+app.get(["/ttt", "/"], function (req, res, next) {
+	res.status(200).render('gamePage',{
+
+		user_data
+
+	});
 })
 
+app.post('/user/add', function (req, res, next) {
+	var person = req.body
+	console.log("== person:", person)
 
-app.get("/ttt", function (req, res, next) {
-	res.status(200).sendFile(__dirname + '/public/index.html');
+	//read leaderboards.json
+	//check for duplicate name
+	if (user_data[person.name]){
+		console.log("user found")
+
+		//update name/score if exists already
+		user_data[person.name].score += person.score
+		sortUserData()
+
+		fs.writeFile(__dirname + "/leaderboards.json",
+		JSON.stringify(user_data, null, 2),
+		function (err) {
+			if(err){
+				res.status(500).send("Error writing new data. Try again")
+			} else{
+//				console.log("before sort funct")
+//				sortUserData()
+				res.status(200).send()
+			}			
+		})
+
+	//else push new name/score to leaderboards.json
+	} else {
+		console.log("new user")
+
+		user_data[person.name] = {
+			"score": person.score
+		}
+		sortUserData()
+		fs.writeFile(__dirname + "/leaderboards.json",
+			JSON.stringify(user_data, null, 2),
+		function (err) {
+			if(err){
+				res.status(500).send("Error writing new data. Try again")
+			} else{
+				res.status(200).send()
+			}			
+		})			
+	}
 })
 
-//Sort LeaderBoard
-var i;
-var j;
-var key;
-for(i = 0; i < user_data.length();i++){
-	key = user_data[i].score
-	for(j = i-1; j >= 0; j--){
-		if(user_data[j].score > key){
-			user_data[j+1] = user_data[j]
+function sortUserData() {
+	var i = 0
+	var name_arr = []
+	var score_arr = []
+	var sorted = false;
+
+	//separate "name" and "score" into two different arrays
+	for (key in user_data) {
+		name_arr.push(key)
+		score_arr.push(user_data[key].score)
+	}
+
+	//sort these two arrays based on a person's score
+	while(!sorted) {
+		sorted = true;
+		for (i = 0; i < name_arr.length; i++) {
+			if (i-1 >= 0) {
+				if (score_arr[i-1] < score_arr[i]) {
+					sorted = false;
+					let sc_temp = score_arr[i-1]
+					score_arr[i-1] = score_arr[i]
+					score_arr[i] = sc_temp
+
+					let nm_temp = name_arr[i-1]
+					name_arr[i-1] = name_arr[i]
+					name_arr[i] = nm_temp
+				}
+			}
 		}
 	}
-	user_data[j+1].score = key
+	//rewrite our user data, recombining name and score arrays
+	user_data = {}
+	for (i = 0; i < name_arr.length; i++) {
+		user_data[name_arr[i]] = {
+			"score": score_arr[i]
+		}
+	}
+
+	console.log(user_data)
 }
+
+app.get('*', function (req, res, next) {
+
+	res.status(404).render('404');
+
+})
 
 app.listen(port, function () {
 	console.log("== Server is listening on port", port);
 })
-
