317d7c11090d32e3b1dd0469b0e5e3c0dd199811
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2021-06-10 13:03:29 -0700

    Copying completed game file and test html page from main

--- public/Game.js
+++ public/Game.js
@@ -1,78 +1,205 @@
-var body = document.body
-var table = document.createElement('table')
-var trs = []
-var tds = []
+var body = document.body;
+var table = document.createElement('table');
+var trs = [];
+var tds = [];
 
-var pTurn = 'X'
-var cTurn = 'O'
-var turn
+var pTurn = 'X';
+var cTurn  = 'O'
 
-var callBack = function(event){
-    console.log(event.target)
-    console.log(event.target.parentNode)
+var turn = pTurn
 
-    console.log(event.target.parentNode.parentNode)
+var threeTd = false
+var cCheck = 0
+var dCheck = 0
 
-    var trNumber = trs.indexOf(event.target.parentNode)
-    var tdNumber = tds[trNumber].indexOf(event.target.parentNode)
+var start
+var end
+var period
+var param
+var digit
+var sub
 
-    if(tds[trNumber][tdNumber].textContent !== ''){
-        console.log('Not Empty')
-    }
-    else{
-        console.log('Empty')
-        tds[trNumber][tdNumber].textContent = pTurn
-        var threeTd = false
-        if(tds[trNumber][0].textContent === pTurn &&
-            tds[trNumber][1].textContent === pTurn &&
-            tds[trNumber][2].textContetn === pTurn){
-                threeTd = true
-        }
-        else if(tds[0][tdNumber].textContent === pTurn &&
-                tds[1][tdNumber].textContent === pTurn && 
-                tds[2][tdNumber].textContent === pTurn){
-                    threeTd = true
-        }
-        else if(Math.abs(trNumber - tdNumber) === 2){
-            if(
-                tds[0][2].textContent === pTurn &&
-                tds[1][1].textContent === pTurn &&
-                tds[2][0].textContent === pTurn){
-                    threeTd = true
-            }
-        }
+var pScore = 0
+var cScore = 0
 
-        if(threeTd === true){
-            window.alert('Player win the game')
-            turn = pTurn
-            tds.forEach(function(trs){
-                trs.forEach(function(td){
-                    td.textContent = ''
-                })
-            })
-        }
-        else{
-            if(turn === pTurn){
-                turn = cTurn
-            }else{
-                turn = pTurn
-            }
-        }
-    }
+start = Date.now()
+
+function Game(event){
+	
+
+	
+	var trNumber = trs.indexOf(event.target.parentNode)
+	var tdNumber = tds[trNumber].indexOf(event.target)
+	
+	if(tds[trNumber][tdNumber].textContent === ""){
+		tds[trNumber][tdNumber].textContent = turn
+		if(threeTd === false){
+			checkWin(trNumber, tdNumber)
+			// checkWinC(trNumber, tdNumber)
+	        turn = cTurn
+
+			if(turn === cTurn){
+				
+				var rand1 = Math.floor(Math.random()*3)
+				var rand2 = Math.floor(Math.random()*3)
+				if(tds[rand1][rand2].textContent === ''){
+					tds[rand1][rand2].textContent = cTurn
+				}
+				else if(tds[rand1][rand2].textContent !== ''){
+					if(tds[0][0].textContent === ''){
+						tds[0][0].textContent = cTurn
+					}
+					else if(tds[0][2].textContent === ''){
+						tds[0][2].textContent = cTurn
+					}
+					else if(tds[2][0].textContent === ''){
+						tds[2][0].textContent = cTurn
+					}
+					else if(tds[2][2].textContent === ''){
+						tds[2][2].textContent = cTurn
+					}	
+					
+					else if(tds[1][0].textContent === ''){
+						tds[1][0].textContent = cTurn
+					}
+					else if(tds[0][1].textContent === ''){
+						tds[0][1].textContent = cTurn
+					}
+					else if(tds[1][1].textContent === ''){
+						tds[1][1].textContent = cTurn
+					}
+					else if(tds[2][1].textContent === ''){
+						tds[2][1].textContent = cTurn
+					}
+					else if(tds[1][2].textContent === ''){
+						tds[1][2].textContent = cTurn
+					}
+				}
+				checkWin(trNumber, tdNumber)
+				// checkWinC()
+				turn = pTurn
+
+			}
+			else if(turn === pTurn){
+				checkWin(trNumber, tdNumber)
+				// checkWinC()
+			}
+		}
+		if(threeTd === true && cCheck === 0){
+			end = Date.now()
+			alert("player is the winner")
+		}
+		else if(cCheck === 1 && threeTd === false){
+			end = Date.now()
+			alert("Cpu is the winner")			
+		}
+		
+		
+	}
+	checkDraw()
+	
+period = end - start
+calScore()
+
+console.log("==DrawCheck: ",dCheck)
+console.log("==time: ",period)
+console.log("===Player: ",pScore)
+console.log("===CPU: ", cScore)
 }
 
-var i
-var j
-for(i = 0; i < 3;i++){
-    var tr = document.createElement('tr')
-    trs.push(tr)
-    tds.push([])
-    for(j = 0; j < 3;j++){
-        var td = document.createElement('td')
-        td.addEventListener('click',callBack)
-        tds[i].push(td)
-        tr.appendChild(td)
+
+
+function checkWin(trNumber, tdNumber ){ 
+	var t
+	for(t = 0; t < 3; t++){
+		if ( tds[t][0].textContent === turn &&
+		tds[t][1].textContent === turn &&
+		tds[t][2].textContent === turn 
+	   	) {
+		   if(turn === pTurn){threeTd = true}
+		   else if(turn === cTurn){cCheck = 1}
+		}
+	   
+			   
+   		else if (tds[0][t].textContent === turn &&
+			tds[1][t].textContent === turn &&
+			tds[2][t].textContent === turn
+		   ){
+			   if(turn === pTurn){threeTd = true}
+			   else if(turn === cTurn){cCheck = 1}
+		}
+	}
+
+	if ( 
+		tds[0][0].textContent === turn &&
+		tds[1][1].textContent === turn &&
+		tds[2][2].textContent === turn
+		) {
+			if(turn === pTurn){threeTd = true}
+			else if(turn === cTurn){cCheck = 1}
+	}
+			
+	else if ( 
+			tds[0][2].textContent === turn &&
+			tds[1][1].textContent === turn &&
+			tds[2][0].textContent === turn
+			) {
+				if(turn === pTurn){threeTd = true}
+				else if(turn === cTurn){cCheck = 1}
+	}
+	
+
+	console.log("===ThreeTd",threeTd)
+	console.log("===c-check: ", cCheck)
+}
+
+function checkDraw(){
+	if(tds[0][0].textContent !== '' && tds[0][1].textContent !== '' && tds[0][2].textContent !== '' && 
+		tds[1][0].textContent !== '' && tds[1][1].textContent !== '' && tds[1][2].textContent !== '' && 
+		tds[2][0].textContent !== '' && tds[2][1].textContent !== '' && tds[2][2].textContent !== '' ){
+			if(threeTd === false && cCheck === 0){
+				dCheck = 1
+			}
+	}
+	if(dCheck === 1){
+		end = Date.now()
+		alert("draw")
+	}
+	else{pSocre = cScore = 0}
+}
+
+function calScore(){
+	param = period/100
+	digit = period/1000
+	sub = digit%5
+	// when the player wins
+	if(threeTd === true){
+		if(param > 0 && param < 10){pScore = 1000 + period}
+		else if(10 <=param && param < 15){pScore = 60 + digit}
+		else if(15 <= param && param < 25){pScore = 50 + digit}
+		else if(25 <= param && param < 30){pScore = 45 + digit}
+		else if(30 <= param && param < 77){pScore = 35 + sub}
+		// Jackpot!!!
+		else if(param === 77){pScore = 7700}
+		else{pScore = 10 + sub }
+
+	}
+	else if(cCheck === 1){cScore++}
+	else if(dCheck === 1){pScore = 3}
+}
+
+
+//Activate the Game
+for (var i = 0; i < 3; i += 1) {
+    var tr = document.createElement('tr');
+    trs.push(tr);
+    tds.push([]);
+    for (var j = 0; j < 3; j += 1) {
+        var td = document.createElement('td');
+        td.addEventListener('click', Game);
+        tds[i].push(td);
+        tr.appendChild(td);
     }
-    table.appendChild(tr)
+    table.appendChild(tr);
 }
 body.appendChild(table)
